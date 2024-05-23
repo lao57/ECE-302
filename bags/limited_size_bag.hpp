9ffe48402b1683421702c7f817ab8245b1b9b21e
Liam <lao57@pitt.edu>
2024-05-23 11:22:33 -0400

    added bags

---
+++ bags/limited_size_bag.hpp
@@ -0,0 +1,44 @@
+#ifndef LIMITED_SIZE_BAG_HPP
+#define LIMITED_SIZE_BAG_HPP
+
+#include "abstract_bag.hpp"
+
+template <typename T>
+class LimitedSizeBag: public AbstractBag<T>
+{
+public:
+  // default constructor
+  LimitedSizeBag();
+  
+  // add an item to the bag
+  bool add(const T & item);
+  
+  // remove an item
+  bool remove(const T & item);
+
+  // check is the bag is empty
+  bool isEmpty() const;
+
+  // get number of items in the bag
+  std::size_t getCurrentSize() const;
+
+  // clear the bag contents
+  void clear();
+
+  // count how many time item occurs in bag
+  std::size_t getFrequencyOf(const T & item) const;
+
+  // check if item is in the bag
+  bool contains(const T& item) const;
+
+  //static variable to store the limited size 
+  static const std::size_t maxsize = 100;
+
+private:
+
+  // TODO
+};
+
+#include "limited_size_bag.tpp"
+
+#endif
---
+++ bags/test_limited_size_bag.cpp
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "limited_size_bag.hpp"
+
+// force template expansion for ints
+template class LimitedSizeBag<int>;
+
+TEST_CASE("Calling all public members", "[LimitedSizeBag]"){
+  LimitedSizeBag<int> b;
+
+  b.add(0);
+  b.remove(0);
+  b.isEmpty();
+  b.getCurrentSize();
+  b.clear();
+  b.getFrequencyOf(0);
+  b.contains(0);
+
+}
